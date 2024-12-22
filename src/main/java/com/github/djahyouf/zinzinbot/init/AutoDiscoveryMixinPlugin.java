78f51977874aefa7ea0e1166a6993caa91a04d03
GitHub Action <action@github.com>
2024-12-22 18:17:08 +0000

    Initialize template repository proper
    Set package name to com.github.djahyouf.zinzinbot
    Set project name to zinzinbot
    Set modid to zinzinbot

--- src/main/java/com/github/djahyouf/zinzinbot/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/djahyouf/zinzinbot/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.djahyouf.zinzinbot.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/djahyouf/zinzinbot/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/djahyouf/zinzinbot/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.djahyouf.zinzinbot.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
